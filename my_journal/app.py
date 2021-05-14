0e04a1b281c2624bd33a7af2fd4e764562201fb7
Joseph Haaga <haaga.joe@gmail.com>
2021-05-14 19:14:02 -0400

    basic test cases passing

--- my_journal/app.py
+++ my_journal/app.py
@@ -1,5 +1,62 @@
 import datetime
+import operator
+from typing import Union
+
+from word2number import w2n
+
+
+scales = {
+    "days": 1,
+    "day": 1,
+    "week": 7,
+    "weeks": 7,
+    "month": 30,
+    "months": 30,
+    "year": 365,
+    "years": 365,
+}
+
+
+def parse_number(number: Union[str, int]) -> int:
+    try:
+        num = int(number)
+    except ValueError:
+        num = w2n.word_to_num(number)
+    return num
 
 
 def parse_english_to_date(english: str) -> datetime.datetime:
-    return False
+    query = english.strip().lower()
+    today = datetime.datetime.now()
+    if query == "yesterday":
+        operation = operator.sub
+        days = 1
+    elif query == "tomorrow":
+        operation = operator.add
+        days = 1
+    elif " ago" in query:
+        operation = operator.sub
+        unit, *_ = [k for k in scales.keys() if k in query]
+        quantity = query.replace(" ago", "").replace(unit, "").strip()
+        number = parse_number(quantity)
+        days_in_unit = scales[unit]
+        days = number * days_in_unit
+    elif " from now" or " from today" in query:
+        operation = operator.add
+        unit, *_ = [k for k in scales.keys() if k in query]
+        quantity = (
+            query.replace(" from now", "")
+            .replace(" from today", "")
+            .replace(unit, "")
+            .strip()
+        )
+        number = parse_number(quantity)
+        days_in_unit = scales[unit]
+        days = number * days_in_unit
+    return operation(today, datetime.timedelta(days=days))
+
+
+#   else if query.startswith("last"):
+#       pass
+#   else if query.startswith("next"):
+#
--- tests/app_test.py
+++ tests/app_test.py
@@ -9,13 +9,20 @@ TODAY = datetime.datetime.now()
 YESTERDAY = TODAY - datetime.timedelta(days=1)
 TWO_DAYS_AGO = TODAY - datetime.timedelta(days=2)
 FOUR_DAYS_AGO = TODAY - datetime.timedelta(days=4)
+TWO_DAYS_FROM_NOW = TODAY + datetime.timedelta(days=2)
+THREE_DAYS_FROM_NOW = TODAY + datetime.timedelta(days=3)
 LAST_WEDNESDAY = None
 
 @pytest.mark.parametrize("english, expected", [
     ("yesterday", YESTERDAY),
     ("2 days ago", TWO_DAYS_AGO),
     ("four days ago", FOUR_DAYS_AGO),
-    ("last wednesday", LAST_WEDNESDAY),
+    ("two days from now", TWO_DAYS_FROM_NOW),
+    ("3 days from now", THREE_DAYS_FROM_NOW)
+#    ("last wednesday", LAST_WEDNESDAY),
 ])
 def test_parse_english_to_date(english, expected):
-    assert parse_english_to_date(english) == expected
+    got = parse_english_to_date(english)
+    got = (got.year, got.month, got.day)
+    expectation = (expected.year, expected.month, expected.day)
+    assert got == expectation
