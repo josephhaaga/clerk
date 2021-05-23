4d1fed0d8db6d1e134273029170d4ae3589f035a
Joseph Haaga <haaga.joe@gmail.com>
2021-05-22 22:18:03 -0400

    add pre-commit

--- tests/parse_test.py
+++ tests/parse_test.py
@@ -13,14 +13,18 @@ TWO_DAYS_FROM_NOW = TODAY + datetime.timedelta(days=2)
 THREE_DAYS_FROM_NOW = TODAY + datetime.timedelta(days=3)
 LAST_WEDNESDAY = None
 
-@pytest.mark.parametrize("english, expected", [
-    ("yesterday", YESTERDAY),
-    ("2 days ago", TWO_DAYS_AGO),
-    ("four days ago", FOUR_DAYS_AGO),
-    ("two days from now", TWO_DAYS_FROM_NOW),
-    ("3 days from now", THREE_DAYS_FROM_NOW)
-#    ("last wednesday", LAST_WEDNESDAY),
-])
+
+@pytest.mark.parametrize(
+    "english, expected",
+    [
+        ("yesterday", YESTERDAY),
+        ("2 days ago", TWO_DAYS_AGO),
+        ("four days ago", FOUR_DAYS_AGO),
+        ("two days from now", TWO_DAYS_FROM_NOW),
+        ("3 days from now", THREE_DAYS_FROM_NOW)
+        #    ("last wednesday", LAST_WEDNESDAY),
+    ],
+)
 def test_parse_english_to_date(english, expected):
     got = parse_english_to_date(english)
     got = (got.year, got.month, got.day)
