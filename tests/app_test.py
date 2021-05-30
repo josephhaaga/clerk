2a41f4f70270ca8f6a54edd71a169cb52aea6504
Joseph Haaga <haaga.joe@gmail.com>
2021-05-29 22:35:17 -0400

    test main application loop

--- tests/app_test.py
+++ tests/app_test.py
@@ -2,7 +2,7 @@ import datetime
 import pytest
 from unittest.mock import patch
 
-from src.app import main
+from src import app
 
 
 TODAY = datetime.datetime.now()
@@ -11,12 +11,21 @@ TWO_DAYS_AGO = TODAY - datetime.timedelta(days=2)
 FOUR_DAYS_AGO = TODAY - datetime.timedelta(days=4)
 TWO_DAYS_FROM_NOW = TODAY + datetime.timedelta(days=2)
 THREE_DAYS_FROM_NOW = TODAY + datetime.timedelta(days=3)
-LAST_WEDNESDAY = None
 
 
-@pytest.mark.parametrize("phrase,date", [("yesterday",), ()])
-@patch("subprocess.run")
-def test_main(subprocess_run):
-    main()
-    breakpoint()
-    assert subprocess_run.called_with()
+@pytest.mark.parametrize(
+    "phrase,date",
+    [
+        ("today", TODAY.strftime("%Y-%m-%d")),
+        ("yesterday", YESTERDAY.strftime("%Y-%m-%d")),
+        ("two days ago", TWO_DAYS_AGO.strftime("%Y-%m-%d")),
+        ("four days ago", FOUR_DAYS_AGO.strftime("%Y-%m-%d")),
+        ("two days from now", TWO_DAYS_FROM_NOW.strftime("%Y-%m-%d")),
+        ("three days from now", THREE_DAYS_FROM_NOW.strftime("%Y-%m-%d")),
+    ],
+)
+@patch("src.app.open_file")
+def test_main(patched_open_file, phrase, date):
+    with patch("sys.argv", [" "] + phrase.split(" ")):
+        app.main()
+    patched_open_file.assert_called_once_with(f"{date}.md")
