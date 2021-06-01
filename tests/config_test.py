44e56a8dbc09ebf0db75c1a55d6e7f07cdf3402c
Joseph Haaga <haaga.joe@gmail.com>
2021-05-31 20:21:50 -0400

    tests pass

--- tests/config_test.py
+++ tests/config_test.py
@@ -1,3 +1,5 @@
+from typing import Mapping
+
 from src.config import dirs
 from src.config import config_file_path
 from src.config import get_config
@@ -5,13 +7,14 @@ from src.config import get_config
 
 def test_dirs_contains_user_config_dir():
     got = dirs()
-    assert isinstance(str, got.user_config_dir)
+    assert isinstance(got.user_config_dir, str)
 
 
 def test_config_file_path_contains_clerk_dot_conf():
     got = config_file_path()
-    assert "clerk.conf" == got[-10:]
+    assert "clerk.conf" == str(got)[-10:]
 
 
 def test_get_config_returns_mapping():
-    assert False == "one"
+    got = get_config()
+    assert isinstance(got, Mapping)
