23f4d90c81b32da3bdeeaba475344c8fc246c5f3
Joseph Haaga <haaga.joe@gmail.com>
2021-06-02 23:03:57 -0400

    factor functions into an Application object

--- src/app.py
+++ src/app.py
@@ -4,7 +4,9 @@ import os
 import pathlib
 import subprocess
 import sys
+from typing import Mapping
 
+from src.config import get_config
 from src.parse import parse_english_to_date
 
 
@@ -18,26 +20,36 @@ FILE_EXTENSION = "md"
 
 def main() -> int:
     # app configuration
+    config = get_config()
+    app = Application(config)
 
     # main loop
     day = "today"  # default to today's journal
     if len(sys.argv) > 1:
         day = " ".join(sys.argv[1:])  # parse date and open that journal
     target_date = parse_english_to_date(day)
-    date_as_filename = convert_to_filename(target_date)
-    open_file(date_as_filename)
+    date_as_filename = app.convert_to_filename(target_date)
+    app.open_file(date_as_filename)
     return 0
 
 
-def open_file(filename: str):
-    file_to_open: pathlib.Path = pathlib.Path(JOURNAL_DIRECTORY, filename)
-    subprocess.run([PREFERRED_EDITOR, file_to_open])
-    return True
+class Application:
+    def __init__(self, config: Mapping):
+        # try to read config, and ensure required values are present
+        # TODO: otherwise, instruct user to setup their config file (`clerk configure`)
+        self.journal_directory = config["DEFAULT"]["journal_directory"]
+        self.preferred_editor = config["DEFAULT"]["preferred_editor"]
+        self.date_format = config["DEFAULT"]["date_format"]
+        self.file_extension = config["DEFAULT"]["file_extension"]
 
+    def open_file(self, filename: str):
+        file_to_open: pathlib.Path = pathlib.Path(self.journal_directory, filename)
+        subprocess.run([self.preferred_editor, file_to_open])
+        return True
 
-def convert_to_filename(target_date: datetime.datetime) -> str:
-    """Convert a datetime.datetime object to a string 'YYYY-MM-DD'"""
-    return f"{target_date.strftime(DATE_FORMAT)}.{FILE_EXTENSION}"
+    def convert_to_filename(self, target_date: datetime.datetime) -> str:
+        """Convert a datetime.datetime object to a string 'YYYY-MM-DD'"""
+        return f"{target_date.strftime(self.date_format)}.{self.file_extension}"
 
 
 if __name__ == "__main__":
--- tests/app_test.py
+++ tests/app_test.py
@@ -2,7 +2,8 @@ import datetime
 import pytest
 from unittest.mock import patch
 
-from src import app
+from src.app import main
+from src.app import Application
 
 
 TODAY = datetime.datetime.now()
@@ -11,6 +12,45 @@ TWO_DAYS_AGO = TODAY - datetime.timedelta(days=2)
 FOUR_DAYS_AGO = TODAY - datetime.timedelta(days=4)
 TWO_DAYS_FROM_NOW = TODAY + datetime.timedelta(days=2)
 THREE_DAYS_FROM_NOW = TODAY + datetime.timedelta(days=3)
+
+EXAMPLE_CONFIG = {
+    "DEFAULT": {
+        "journal_directory": "./my-journal-directory",
+        "preferred_editor": "vi",
+        "date_format": "%Y-%m-%d",
+        "file_extension": "md",
+    }
+}
+
+
+@pytest.fixture(scope="session")
+def example_app():
+    return Application(EXAMPLE_CONFIG)
+
+
+@patch("subprocess.run")
+def test_application_open_file(patched_subprocess_run, example_app):
+    filename = "1234.md"
+    example_app.open_file(filename)
+    patched_subprocess_run.assert_called_once()
+
+
+@pytest.mark.parametrize(
+    "date, filename",
+    [
+        (TODAY, f"{TODAY.strftime('%Y-%m-%d')}.md"),
+        (YESTERDAY, f"{YESTERDAY.strftime('%Y-%m-%d')}.md"),
+    ],
+)
+def test_application_convert_to_filename(date, filename, example_app):
+    got = example_app.convert_to_filename(date)
+    assert got == filename
+
+
+@pytest.mark.skip
+def test_application_creation_fails_with_missing_config_item():
+    # xfail when Application is initialized with a ConfigParser (a dict) missing `journal_directory`
+    assert False
 
 
 @pytest.mark.parametrize(
@@ -24,8 +64,11 @@ THREE_DAYS_FROM_NOW = TODAY + datetime.timedelta(days=3)
         ("three days from now", THREE_DAYS_FROM_NOW.strftime("%Y-%m-%d")),
     ],
 )
-@patch("src.app.open_file")
-def test_main(patched_open_file, phrase, date):
-    with patch("sys.argv", [" "] + phrase.split(" ")):
-        app.main()
+@patch("src.app.Application.open_file")
+def test_main_loop(patched_open_file, phrase, date):
+    # mock the `get_config` with a basic config
+    # patch `Application.open_file` and assert it's called as expected
+    with patch("src.app.get_config", lambda: EXAMPLE_CONFIG):
+        with patch("sys.argv", [" "] + phrase.split(" ")):
+            main()
     patched_open_file.assert_called_once_with(f"{date}.md")
