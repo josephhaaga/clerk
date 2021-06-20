e98eb3faf64d03b377104361ba8dd5f4518840bd
Joseph Haaga <haaga.joe@gmail.com>
2021-06-20 12:06:59 -0400

    add hooks to support custom extensions (#9)
    * add hooks implementation
    
    * bump package version

--- TODO.md
+++ TODO.md
@@ -1,4 +1,4 @@
-## Todos
+# Todos
 
 - [X] create basic workflow to open appropriate journal files
 - [X] test the main application loop (`app.main()`)
@@ -7,6 +7,7 @@
 - [ ] add "create new journal" functionality with a sensible templating solution
     - add prehook and posthook functionality around the `subprocess.run` call
         - then add a custom prehook that, if file is new, inserts a rendered template
+
 - [ ] add a `sample-config` command that prints an example config
     - README should instruct users to `clerk sample-config > /path/to/config/file`
     - since our config files are hidden in appdirs, maybe we should have a `generate-config` command that writes it _for_ them?
@@ -18,3 +19,20 @@
 - [ ] publish package
     - create a `console_scripts` "alias" for `journal`
     - setup.cfg, pyproject.toml
+- [ ] create a `clerk-extension` starter template repo, with documentation on how to publish
+
+## Hooks functionality
+- [X] add hooks functionality to main function
+- [X] update config to parse hook specifications from config files
+- [X] figure out how hooks get installed
+- [X] write a simple hook to append a timestamp
+- Will 0.0.2 automatically read the 0.0.2/clerk.conf file? Build the package, make a new virtualenv, and install to find out
+
+
+## More extensions
+- use `cowsay` to print cow, dinosaur etc.
+- use cowsay to print google tasks
+- blackendocs to blacken any python snippets
+- autosave, which will copy the tempfile back even if I don't save from my text editor
+- fortune cookie, which adds a fortune to your journal
+- forecast, adds weather to your journal
--- clerk/app.py
+++ clerk/app.py
@@ -1,9 +1,14 @@
-# Move journal.sh logic into here
 import datetime
 import os
+import hashlib
+import importlib.metadata
 import pathlib
+import shutil
 import subprocess
 import sys
+import tempfile
+from typing import Callable
+from typing import Sequence
 from typing import Mapping
 
 from clerk.config import config_file_path
@@ -12,9 +17,16 @@ from clerk.parse import parse_english_to_date
 
 
 def main() -> int:
-    # app configuration
+    try:
+        eps = importlib.metadata.entry_points()["clerk.extensions"]
+        # https://youtu.be/fY3Y_xPKWNA?t=717
+        _extensions = {entrypoint.name: entrypoint for entrypoint in eps}
+    except KeyError:
+        # no plugins installed
+        _extensions = {}
+
     config = get_config()
-    app = Application(config)
+    app = Application(config, _extensions)
 
     # main loop
     day = "today"  # default to today's journal
@@ -22,33 +34,96 @@ def main() -> int:
         day = " ".join(sys.argv[1:])  # parse date and open that journal
     target_date = parse_english_to_date(day)
     date_as_filename = app.convert_to_filename(target_date)
-    app.open_file(date_as_filename)
+    app.open_journal(date_as_filename)
     return 0
 
 
 class Application:
-    def __init__(self, config: Mapping):
+    def __init__(self, config: Mapping, extensions: Mapping):
         # try to read config, and ensure required values are present
         # TODO: otherwise, instruct user to setup their config file (`clerk configure`)
         try:
-            self.journal_directory = config["DEFAULT"]["journal_directory"]
-            self.preferred_editor = config["DEFAULT"]["preferred_editor"]
-            self.date_format = config["DEFAULT"]["date_format"]
-            self.file_extension = config["DEFAULT"]["file_extension"]
+            self.config = config
+            self.extensions = extensions
+            self.journal_directory = self.config["DEFAULT"]["journal_directory"]
+            self.preferred_editor = self.config["DEFAULT"]["preferred_editor"]
+            self.date_format = self.config["DEFAULT"]["date_format"]
+            self.file_extension = self.config["DEFAULT"]["file_extension"]
+            self.hooks = {
+                "NEW_JOURNAL_CREATED": self._get_callbacks_for_hook(
+                    "NEW_JOURNAL_CREATED"
+                ),
+                "JOURNAL_OPENED": self._get_callbacks_for_hook("JOURNAL_OPENED"),
+                "JOURNAL_SAVED": self._get_callbacks_for_hook("JOURNAL_SAVED"),
+                "JOURNAL_CLOSED": self._get_callbacks_for_hook("JOURNAL_CLOSED"),
+            }
         except KeyError as e:
             print(
                 f"Your configuration at {config_file_path()} is missing a key '{e.args[0]}'"
             )
             exit(1)
 
-    def open_file(self, filename: str):
+    def _get_callbacks_for_hook(self, hook_name: str) -> Sequence[Callable]:
+        if "hooks" not in self.config:
+            return []
+        if hook_name not in self.config["hooks"]:
+            return []
+        try:
+            return [
+                self.extensions[ext_name]
+                for ext_name in self.config["hooks"][hook_name].split("\n")
+                if ext_name != ""
+            ]
+        except KeyError as e:
+            print(
+                f"Couldn't find plugin '{e.args[0]}' installed; please check your configuration at {config_file_path()}"
+            )
+            exit(1)
+
+    def _apply_callbacks_for_hook(self, hook_name: str, filename: pathlib.Path):
+        # applies a list of handler functions to a file
+        with open(filename, "r+") as f:
+            for callback in self.hooks[hook_name]:
+                f.seek(0)
+                data = f.readlines()
+                results = callback.load()(data)
+                if results:
+                    f.seek(0)
+                    f.writelines(results)
+                    print(f"{callback.name} ran; changes applied!")
+                else:
+                    print(f"{callback.name} ran; no changes made")
+                f.truncate()
+
+    def open_journal(self, filename: str):
         file_to_open: pathlib.Path = pathlib.Path(self.journal_directory, filename)
-        subprocess.run([self.preferred_editor, file_to_open])
-        return True
+        with tempfile.NamedTemporaryFile() as journal:
+            if not pathlib.Path(file_to_open).exists():
+                self._apply_callbacks_for_hook("NEW_JOURNAL_CREATED", journal.name)
+            else:
+                shutil.copy(file_to_open, journal.name)
+
+            self._apply_callbacks_for_hook("JOURNAL_OPENED", journal.name)
+
+            old_hash = get_file_hash(journal.name)
+            subprocess.run([self.preferred_editor, journal.name])
+            new_hash = get_file_hash(journal.name)
+
+            if old_hash != new_hash:
+                self._apply_callbacks_for_hook("JOURNAL_SAVED", journal.name)
+                shutil.copy(journal.name, file_to_open)
+
+            self._apply_callbacks_for_hook("JOURNAL_CLOSED", journal.name)
+            return True
 
     def convert_to_filename(self, target_date: datetime.datetime) -> str:
         """Convert a datetime.datetime object to a string 'YYYY-MM-DD'"""
         return f"{target_date.strftime(self.date_format)}.{self.file_extension}"
+
+
+def get_file_hash(filename):
+    with open(filename, "r") as f:
+        return hashlib.md5(f.read().encode("utf-8")).hexdigest()
 
 
 if __name__ == "__main__":
