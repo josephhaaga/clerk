ed485cabb7ff7f464ddf05ae3eff9154f2fd8bb9
Joseph Haaga <haaga.joe@gmail.com>
2021-06-19 20:32:17 -0400

    refactor and add placeholder for hooks

--- clerk/app.py
+++ clerk/app.py
@@ -1,10 +1,11 @@
-# Move journal.sh logic into here
 import datetime
 import os
-from hashlib import sha256
+import hashlib
 import pathlib
+import shutil
 import subprocess
 import sys
+import tempfile
 from typing import Mapping
 
 from clerk.config import config_file_path
@@ -13,7 +14,6 @@ from clerk.parse import parse_english_to_date
 
 
 def main() -> int:
-    # app configuration
     config = get_config()
     app = Application(config)
 
@@ -23,7 +23,7 @@ def main() -> int:
         day = " ".join(sys.argv[1:])  # parse date and open that journal
     target_date = parse_english_to_date(day)
     date_as_filename = app.convert_to_filename(target_date)
-    app.open_file(date_as_filename)
+    app.open_journal(date_as_filename)
     return 0
 
 
@@ -42,45 +42,36 @@ class Application:
             )
             exit(1)
 
-    def open_file(self, filename: str):
+    def open_journal(self, filename: str):
         file_to_open: pathlib.Path = pathlib.Path(self.journal_directory, filename)
-        if not file_to_open.exists():
-            # NEW_JOURNAL_CREATED
-            print("Creating new journal")
-            hash_before = ""
-        else:
-            hash_before = hash_file(file_to_open)
-        # JOURNAL_OPENED
-        print("journal opened")
-        subprocess.run([self.preferred_editor, file_to_open])
+        with tempfile.NamedTemporaryFile() as journal:
+            if not pathlib.Path(file_to_open).exists():
+                print("NEW_JOURNAL_CREATED")
+            else:
+                shutil.copy(file_to_open, journal.name)
 
-        hash_after = ""
-        if file_to_open.exists():
-            hash_after = hash_file(file_to_open)
+            old_hash = get_file_hash(journal.name)
 
-        if hash_before != hash_after:
-            # JOURNAL_SAVED
-            print("journal saved")
+            print("JOURNAL_OPENED")
+            subprocess.run([self.preferred_editor, journal.name])
 
-        # JOURNAL_CLOSED
-        print("journal closed")
+            new_hash = get_file_hash(journal.name)
 
-        return True
+            if old_hash != new_hash:
+                print("JOURNAL_SAVED")
+                shutil.copy(journal.name, file_to_open)
+
+            print("JOURNAL_CLOSED")
+            return True
 
     def convert_to_filename(self, target_date: datetime.datetime) -> str:
         """Convert a datetime.datetime object to a string 'YYYY-MM-DD'"""
         return f"{target_date.strftime(self.date_format)}.{self.file_extension}"
 
 
-def hash_file(filepath: pathlib.Path) -> str:
-    h = sha256()
-    with open(filepath, "rb") as f:
-        while True:
-            chunk = f.read(h.block_size)
-            if not chunk:
-                break
-            h.update(chunk)
-        return h.hexdigest()
+def get_file_hash(filename):
+    with open(filename, "r") as f:
+        return hashlib.md5(f.read().encode("utf-8")).hexdigest()
 
 
 if __name__ == "__main__":
