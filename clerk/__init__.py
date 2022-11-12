b57a255687e0a423b83dde3e2824fd3702eb1e60
Joseph Haaga <haaga.joe@gmail.com>
2022-11-12 11:50:15 -0500

    fix broken test suite

--- clerk/__init__.py
+++ clerk/__init__.py
@@ -1,4 +1,8 @@
 """Manage your daily journals"""
+from importlib.metadata import PackageNotFoundError
 from importlib.metadata import version
 
-__version__ = version(__name__)
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = "0.0.0"  # fallback if package hasn't been `pip install`ed
